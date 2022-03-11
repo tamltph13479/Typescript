ccf0c358c4dd2a48735c79ed54c5d2297e82c439
tamltph13479 <thetam1002@gmail.com>
2022-03-11 21:20:15 +0700

    buoi 3

---
+++ dist/app.js
@@ -0,0 +1,6 @@
+"use strict";
+function sum(numA, numB) {
+    return numA + numB;
+}
+console.log(sum(220, 20));
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,4 @@
+function sum(numA: number, numB: number) {
+    return numA + numB
+}
+console.log(sum(220, 20))
